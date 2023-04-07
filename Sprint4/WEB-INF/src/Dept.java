9c158fb994b171349710c7f16ee96f36a062f08b
Heritoky007 <126861936+Heritoky007@users.noreply.github.com>
2023-04-07 11:57:17 +0300

    sprint4

---
+++ Sprint4/WEB-INF/src/Dept.java
@@ -0,0 +1,14 @@
+package models;
+
+import etu1894.annotation.*;
+
+public class Dept {
+    @Annotation_url(url = "all-dept")
+    public void find_all_dept() {
+
+    }
+
+    public void insert() {
+
+    }
+}
